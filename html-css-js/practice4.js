196f093938a8ae44d15cfae35e2092b8aa7f008a
harshityadav-7 <harshityadav2002@icloud.com>
2025-08-29 21:04:06 +0530

    add querySelectorAll(button)

--- html-css-js/practice4.js
+++ html-css-js/practice4.js
@@ -1,6 +1,17 @@
-function gameClicked(element,category){
+/* function gameClicked(element,category){
   const clickedClass=`${category}-clicked`;
   if(element.classList.contains(clickedClass)){
     element.classList.remove(clickedClass);
   }else element.classList.add(clickedClass);
+} */
+
+function gameClicked(element,category){
+  const allButtons =document.querySelectorAll('button');
+
+  allButtons.forEach(button =>{
+    button.classList.remove('game-clicked','music-clicked','tech-clicked');
+  });
+
+  const clickedClass=`${category}-clicked`;
+  element.classList.add(clickedClass);
 }
