7948fe658680227c3d1ae0a8fcf7f596794ea77c
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-01 08:13:40 +0530

    loops practice code

--- 11-arrays-and-loops/Practice/practice1.js
+++ 11-arrays-and-loops/Practice/practice1.js
@@ -1,8 +1,23 @@
 const arr=[10,20,30];
 console.log(arr);
 arr[arr.length-1]=99;
-console.log(arr);
 
 function getLastValue(arr){
   console.log(arr[arr.length-1]);
+}
+getLastValue(arr);
+
+function getSwap(arr){
+  const temp=arr[0];
+  arr[0]=arr[arr.length-1];
+  arr[arr.length-1]=temp;
+}
+getSwap(arr);
+console.log(arr);
+
+for(let i=0; i<=10; i+=2){
+  console.log(i);
+}
+for(let i=5; i>=0; i--){
+  console.log(i);
 }
