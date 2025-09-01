355a74d2de10e26c1f2e9541956fadf98af8f8d3
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-01 11:57:28 +0530

    use forEach() in place of for loop

---
+++ 12-advanced-functions/12-todo-list.js
@@ -0,0 +1,39 @@
+const todoList =JSON.parse(localStorage.getItem('todoList')) || [{name:'make dinner',
+  dueDate:'2022-12-22'
+}, {
+  name:'wash dishes',
+  dueDate:'2022-12-22'
+}];
+
+renderTodoList();
+function renderTodoList(){
+  let todoListHTML='';
+
+  todoList.forEach(function(todoObject,index){
+    const {name, dueDate} = todoObject;
+    const html =`
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-todo-button" onclick="
+        todoList.splice(${index},1);
+        localStorage.setItem('todoList',JSON.stringify(todoList));
+        renderTodoList();
+      ">Delete</button>
+    `;
+    todoListHTML+=html;
+  });
+  document.querySelector('.js-todo-list').innerHTML=todoListHTML;
+}
+
+function addTodo(){
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+  const dateInputElement=document.querySelector('.js-due-date-input');
+  const dueDate=dateInputElement.value;
+  todoList.push({name,dueDate});//since key and value are same in name
+
+
+  inputElement.value='';
+
+  renderTodoList();
+}
