47402f4c894c1e357f7ca6b185db70763ef47a0d
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-01 12:41:55 +0530

    add practice functions

--- 12-advanced-functions/Practice/practice1.js
+++ 12-advanced-functions/Practice/practice1.js
@@ -12,3 +12,26 @@ function fun(){
 }
 runTwice(fun);
 
+
+function touched(){
+  const docElement = document.querySelector('.start-button');
+  if(docElement.innerHTML === 'Start'){
+    docElement.innerHTML ='Loading!';
+    setTimeout(function(){
+      docElement.classList.add('finished-button');
+      docElement.innerHTML ='Finished!';
+    },1000);
+  }else{
+    docElement.classList.remove('finished-button');
+    docElement.innerHTML ='Start';
+  }
+}
+
+function addCart(){
+  const docElement = document.querySelector('.message-container');
+  docElement.innerHTML='Added';
+
+  setTimeout(function(){
+    docElement.innerHTML='';
+  },2000);
+}
