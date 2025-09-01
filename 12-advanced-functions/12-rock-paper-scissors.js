bdcbccf1c09970c62e3b16af240c6c6a90f53613
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-01 11:56:51 +0530

    use forEach() in place of for loop

---
+++ 12-advanced-functions/12-rock-paper-scissors.js
@@ -0,0 +1,90 @@
+let score = JSON.parse(localStorage.getItem('score')) ||{
+  wins:0,
+  losses:0,
+  ties:0
+};
+
+updateScoreElement();
+
+function pickComputerMove(){
+  let computerMove='';
+  const randomNumber=Math.random();
+  if(randomNumber >= 0 && randomNumber < 1/3){
+    computerMove = 'rock';
+  }else if(randomNumber >= 1/3 && randomNumber < 2/3){
+    computerMove = 'paper';
+  }else if(randomNumber >= 2/3 && randomNumber < 1){
+    computerMove = 'scissors';
+  }
+  return computerMove;
+}
+
+function playGame(playerMove){
+  const computerMove = pickComputerMove();
+  let result='';
+
+  if(playerMove === 'scissors'){
+    if(computerMove === 'rock'){
+      result = 'You lose.';
+    }else if(computerMove == 'paper'){
+      result='You win.';
+    }else if(computerMove === 'scissors'){
+      result='Tie.';
+    }
+
+  } else if(playerMove === 'rock'){
+    if(computerMove === 'rock'){
+      result = 'Tie.';
+    }else if(computerMove == 'paper'){
+      result='You lose.';
+    }else if(computerMove === 'scissors'){
+      result='You win.';
+    }
+
+  } else if(playerMove === 'paper'){
+    if(computerMove === 'rock'){
+      result = 'You win.';
+    }else if(computerMove == 'paper'){
+      result='Tie.';
+    }else if(computerMove === 'scissors'){
+      result='You lose.';
+    }
+  }
+
+  if(result === 'You win.'){
+    score.wins++;
+  }else if(result === 'Tie.'){
+    score.ties++;
+  }else{
+    score.losses++;
+  }
+
+  localStorage.setItem('score',JSON.stringify(score));
+
+  updateScoreElement();
+  document.querySelector('.js-result').innerHTML=result;
+  document.querySelector('.js-userComputerMove').innerHTML=`You
+    <img src="images/${playerMove}-emoji.png" class="move-icon">
+    <img src="images/${computerMove}-emoji.png" class="move-icon">
+    Computer`;
+}
+
+function updateScoreElement(){
+  document.querySelector('.js-score').innerHTML=
+`Wins:${score.wins}, Losses:${score.losses}, Ties:${score.ties}`;
+}
+
+let isAutoPlaying = false;
+let intervalId;
+function autoplay(){
+  if(!isAutoPlaying){
+    intervalId = setInterval(function(){
+      const playerMove=pickComputerMove();
+      playGame(playerMove);
+    },1000);
+    isAutoPlaying=true;
+  }else{
+    clearInterval(intervalId);
+    isAutoPlaying=false;
+  }
+}
