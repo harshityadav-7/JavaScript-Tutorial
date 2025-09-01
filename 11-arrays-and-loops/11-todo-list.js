a3fca7f86056e2259ff91804833240eb815a7aac
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-01 08:52:50 +0530

    make use of object in place of strings inside array

--- 11-arrays-and-loops/11-todo-list.js
+++ 11-arrays-and-loops/11-todo-list.js
@@ -1,13 +1,19 @@
-const todoList =[];//empty array
+const todoList =[{name:'make dinner',
+  dueDate:'2022-12-22'
+}, {
+  name:'wash dishes',
+  dueDate:'2022-12-22'
+}];
 
 renderTodoList();
 function renderTodoList(){
   let todoListHTML='';
   for(let index=0; index<todoList.length; index++){
-    const todo=todoList[index];
+    const todoObject=todoList[index];
+    const {name, dueDate} = todoObject;
     const html =`
       <p>
-        ${todo}
+        ${name} ${dueDate}
         <button onclick="
           todoList.splice(${index},1);
           renderTodoList();
