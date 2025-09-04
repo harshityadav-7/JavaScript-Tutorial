bcbcbf3af5f2a410ca94eacbda10767a86f8f153
harshityadav-7 <harshityadav2002@icloud.com>
2025-09-04 07:50:13 +0530

    use map to increase every value by num

--- 12-advanced-functions/Practice/practice3.js
+++ 12-advanced-functions/Practice/practice3.js
@@ -1,1 +1,13 @@
 const multiply = (a,b)=>a*b;
+console.log(multiply(2,3));
+
+const countPositive = [-2,3,-5,7,10].filter((value)=> value>0).length;
+console.log(countPositive);
+
+
+const addNum =(array,num)=>{
+  return array.map((value,index)=>{
+    return value*num;
+  });
+};
+console.log(addNum([1,2,3],2));
